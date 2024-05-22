f3dfe9a23883e9d9e79f1fcd892ee8b0ba084ba8
Erol Kaan Bostan <bostanerolkaan@gmail.com>
2024-05-21 17:07:28 -0700

    deleteBookById Tests Updated

--- tests/deleteBookById.test.js
+++ tests/deleteBookById.test.js
@@ -1,26 +1,61 @@
-const request = require('supertest');
-const app = require('../NodeJS_');
+const request = require("supertest");
+const app = require("../NodeJS_");
 let server;
 
 beforeAll(() => {
-  server = require('../NodeJS_Server');
+  server = require("../NodeJS_Server");
 });
 
 afterAll((done) => {
   server.close(done);
 });
 
-describe('DELETE /books/:id', () => {
-  beforeAll(() => {
+describe("DELETE /books/:id", () => {
+  beforeEach(() => {
+    app.resetBooks();
     return request(app)
-      .post('/books')
-      .send({ id: 1, title: 'Book 1', author: 'Author 1', published_date: '2022-01-01', price: 9.99 })
+      .post("/books")
+      .send({
+        id: 1,
+        title: "Book 1",
+        author: "Author 1",
+        published_date: "2024-08-04",
+        price: 10.99,
+      })
       .expect(201);
   });
 
-  test('should delete a book by ID', () => {
+  test("should delete a book by ID", () => {
     return request(app)
-      .delete('/books/1')
-      .expect(200);
+      .delete("/books/1")
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toEqual({ message: "Book deleted successfully" });
+        //To check if it is actually deleted
+        return request(app).get("/books/1").expect(404);
+      });
+  });
+
+  test("should return a 404 if a book id does not exist in books", () => {
+    return request(app)
+      .delete("/books/1000000")
+      .expect(404)
+      .then((response) => {
+        expect(response.body).toEqual({ message: "Book not found" });
+      });
+  });
+
+  test("should not try to delete a book that is already deleted", () => {
+    return request(app)
+      .delete("/books/1")
+      .expect(200)
+      .then(() => {
+        return request(app)
+          .delete("/books/1")
+          .expect(404)
+          .then(response => {
+            expect(response.body).toEqual({ message: "Book not found" });
+          });
+      });
   });
 });
